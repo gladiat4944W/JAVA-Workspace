f91f15ff75648aed467c48cc4d8553d70b52f215
Davis J.Johney <76844714+gladiat4944W@users.noreply.github.com>
2025-02-10 12:11:05 +0530

    Add files via upload

---
+++ degree2.java
@@ -0,0 +1,17 @@
+// Java Program to Convert Fahrenheit into Celsius
+
+class degree2 {
+
+    public static void main(String[] args)
+    {
+        // temperature in fahrenheit
+        double f = 50.0, c = 0.0;
+
+        // calculate celsius temperature
+        c = (f - 32) / 1.8;
+
+        // print the celsius temperature
+        System.out.println(
+            "value of temperature in celsius:" + c);
+    }
+}
