5a2bf48252493c4e1866db85aa5fecb6df0dad10
Davis J.Johney <76844714+gladiat4944W@users.noreply.github.com>
2025-02-10 12:13:30 +0530

    Add files via upload

---
+++ example1.java
@@ -0,0 +1,7 @@
+class example1{
+    public static void main(String[] args){
+System.out.println("hello world");
+System.out.println("welcome to java programing");
+System.out.println("learning of java is very useful");
+}
+}
---
+++ example2.java
@@ -0,0 +1,5 @@
+class exapmle2{
+public static void main(String[] arg){
+System.out.println("this is roll no 24042");
+ }
+}
